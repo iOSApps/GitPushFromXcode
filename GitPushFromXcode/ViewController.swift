d509d25dfeda2a82e44a2aec72cb0eda1cf93647
sandeepchalla <sandeep.challa@rblfinserve.com>
2023-06-01 13:25:47 +0530

    Initial Commit

---
+++ GitPushFromXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitPushFromXcode
+//
+//  Created by sandeepchalla on 01/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
